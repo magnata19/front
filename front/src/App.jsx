8b01a7c80b51bce0c1d2805f1bca14446dece51e
Davidson <dehpacifico@gmail.com>
2023-12-09 19:05:04 -0300

    Inciando frontend do projeto spring-react

---
+++ front/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Formulario from "./Formulario";
+import Tabela from "./Tabela";
+
+function App() {
+  return <>
+    <Formulario />
+    <Tabela />
+  </>;
+}
+
+export default App;
---
+++ front/src/Tabela.jsx
@@ -0,0 +1,25 @@
+
+const Tabela = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nome</th>
+          <th>Marca</th>
+          <th>Selecionar</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default Tabela
