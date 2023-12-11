58d6bd5357f8b1c42112824acaf521b273a8d217
Davidson <dehpacifico@gmail.com>
2023-12-11 19:25:23 -0300

    exibindo produtos na tabela

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Formulario from "./Formulario";
 import Tabela from "./Tabela";
 
 function App() {
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
 
-  const[btnCadastrar, setBtnCadastrar] = useState(true);
-  return <>
-    <Formulario botao={btnCadastrar}/>
-    <Tabela />
-  </>;
+  //use effect
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido))
+  }, []);
+
+  return (
+    <>
+      <Formulario botao={btnCadastrar}/>
+      <Tabela vetor={produtos}/>
+    </>
+  );
 }
 
 export default App;
