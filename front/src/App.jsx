be469ee7d81cedfb7f39003b74c4fb54f809ef1e
Davidson <dehpacifico@gmail.com>
2023-12-11 18:59:12 -0300

    visibilidade de botoes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 import Formulario from "./Formulario";
 import Tabela from "./Tabela";
 
 function App() {
+
+  const[btnCadastrar, setBtnCadastrar] = useState(true);
   return <>
-    <Formulario />
+    <Formulario botao={btnCadastrar}/>
     <Tabela />
   </>;
 }
--- front/src/Formulario.jsx
+++ front/src/Formulario.jsx
@@ -1,13 +1,18 @@
-const Formulario = () => {
+const Formulario = ({ botao }) => {
   return (
     <form>
       <input type="text" placeholder="Nome" />
       <input type="text" placeholder="Marca" />
 
-      <input type="button" value="Cadastrar" className="btn btn-primary" />
-      <input type="button" value="Alterar" className="btn btn-warning" />
-      <input type="button" value="Remover" className="btn btn-danger" />
-      <input type="button" value="Cancelar" className="btn btn-secondary" />
+      {botao ? (
+        <input type="button" value="Cadastrar" className="btn btn-primary" />
+      ) : (
+        <div>
+          <input type="button" value="Alterar" className="btn btn-warning" />
+          <input type="button" value="Remover" className="btn btn-danger" />
+          <input type="button" value="Cancelar" className="btn btn-secondary" />
+        </div>
+      )}
     </form>
   );
 };
